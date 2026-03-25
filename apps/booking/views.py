c5ce718c4f4226386ee9f4153e1bb549451bdf27
dmitrylunar <dmitrylunar@gmail.com>
2026-03-25 20:25:44 +0300

    Add a booking form

--- apps/booking/views.py
+++ apps/booking/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import OrderForm
 
 # Create your views here.
+def booking(request):
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('booking')
+    else:
+        form = OrderForm()
+    return render(request, 'booking/booking.html', {'form': form})
