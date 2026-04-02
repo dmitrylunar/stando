cd182af34eded785ab8cbdf5789da83207b9bdd6
dmitrylunar <dmitrylunar@gmail.com>
2026-04-02 06:31:08 +0300

    Add dashboard view, style base layout and homepage

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.contrib.auth.views import  LoginView, LogoutView
-from .views import registration
+from .views import registration, dashboard
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('registration/', registration, name='registration')
+    path('registration/', registration, name='registration'),
+    path('dashboard/', dashboard, name='dashboard')
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from apps.booking.models import Order
+from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 # Create your views here.
 def registration(request):
@@ -11,3 +14,9 @@ def registration(request):
     else:
         form = UserCreationForm()
     return render(request, 'accounts/register.html', {'form': form})
+
+@login_required
+def dashboard(request):
+    active = Order.objects.filter(status__in=['new', 'confirmed'], user=request.user, datetime__gte=datetime.now())
+    archive = Order.objects.filter(status__in=['done', 'cancelled'], user=request.user, datetime__lt=datetime.now())
+    return render(request, 'accounts/dashboard.html', context={'archive': archive, 'active': active})
