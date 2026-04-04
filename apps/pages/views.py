3a226fbc79d6577caaf1ec0afd5b9a9df46cae07
dmitrylunar <dmitrylunar@gmail.com>
2026-04-04 14:56:38 +0300

    Add services and portfolio pages, update navbar links

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'pages/home.html')
+
+def service(request):
+    return render(request, 'pages/service.html')
+
+def portfolio(request):
+    return render(request, 'pages/portfolio.html')
