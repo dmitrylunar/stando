b86d8bc1c4e5e3720cb7c687160d94018914a148
dmitrylunar <dmitrylunar@gmail.com>
2026-04-04 18:17:43 +0300

    Add services page with Service model and cards layout

--- apps/booking/models.py
+++ apps/booking/models.py
@@ -15,12 +15,17 @@ STATUS_CHOICES = [
     ('cancelled', 'Отменен')
 ]
 
+class Service(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=20)
     email = models.CharField(max_length=100)
-    service = models.CharField(max_length=20, choices=SERVICE_CHOISES, default='rec')
+    service = models.ForeignKey(Service, on_delete=models.SET_NULL, null=True)
     datetime = models.DateTimeField()
     comment = models.TextField(blank=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new')
--- apps/pages/views.py
+++ apps/pages/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
+from apps.booking.models import Service
 
 # Create your views here.
 def home(request):
     return render(request, 'pages/home.html')
 
 def service(request):
-    return render(request, 'pages/service.html')
+    service = Service.objects.all()
+    return render(request, 'pages/service.html', context={'service': service})
 
 def portfolio(request):
     return render(request, 'pages/portfolio.html')
