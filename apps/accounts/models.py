02569c84e2e66d6b3fc3979164006943b97ef03e
dmitrylunar <dmitrylunar@gmail.com>
2026-04-04 14:25:22 +0300

    Add user profile with edit mode to dashboard

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class UserProfile(models.Model):
+    name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=20)
+    discount = models.FloatField(default=0)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
+
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.forms import UserCreationForm
 from apps.booking.models import Order
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
+from .forms import UserProfileForm
+from .models import UserProfile
 
 # Create your views here.
 def register(request):
@@ -19,4 +21,12 @@ def register(request):
 def dashboard(request):
     active = Order.objects.filter(status__in=['new', 'confirmed'], user=request.user, datetime__gte=datetime.now())
     archive = Order.objects.filter(status__in=['done', 'cancelled'], user=request.user, datetime__lt=datetime.now())
-    return render(request, 'accounts/dashboard.html', context={'archive': archive, 'active': active})
+    profile, created = UserProfile.objects.get_or_create(user=request.user)
+    if request.method == 'GET':
+        form = UserProfileForm(instance=profile)
+    elif request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard')
+    return render(request, 'accounts/dashboard.html', context={'archive': archive, 'active': active, 'form': form})
